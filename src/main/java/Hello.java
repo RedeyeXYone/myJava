8ebb30cfce0e82ad7ff40de81aa5b20058ec4e68
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-03-13 15:30:09 +0900

    System.out.println("저 너머에 천국과 지옥을 초월한 무언가가 있어");

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args){
 
         System.out.println("The west once had wonders once had angel too");
+        System.out.println("저 너머에 천국과 지옥을 초월한 무언가가 있어");
     }
 }
