6cd60ab045d3653660eea226d35a2186a9dac80f
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-03-30 20:45:23 +0900

    public class Exam01 {     public static void main(String[] args) {         String str = "Hello BYE";
            str = str.replaceAll(" ", "");
    
            str = str.toLowerCase();
    
            System.out.println(str);
    
        }
    }

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,12 @@
+public class Exam01 {
+    public static void main(String[] args) {
+        String str = "Hello BYE";
+
+        str = str.replaceAll(" ", "");
+
+        str = str.toLowerCase();
+
+        System.out.println(str);
+
+    }
+}
