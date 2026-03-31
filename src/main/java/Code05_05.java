33f76d2fcdd5f01a995ad51ab4f156dbfa252e07
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-03-31 11:06:27 +0900

    import java.util.Scanner;
    public class Code05_05 {
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
            int num;
    
            System.out.println("숫자 입력 ==> ");
            num = sc.nextInt();
    
            if(num%2 == 0)
                System.out.println("짝수임");
            else
                System.out.println("홀수임");
    
            sc.close();
        }
    }

---
+++ src/main/java/Code05_05.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Code05_05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.println("숫자 입력 ==> ");
+        num = sc.nextInt();
+
+        if(num%2 == 0)
+            System.out.println("짝수임");
+        else
+            System.out.println("홀수임");
+
+        sc.close();
+    }
+}
+
+
