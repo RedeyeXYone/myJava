052a74bb846bcae55fb01b900998108dedca42ba
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-03-31 10:33:06 +0900

    public class Code05_04 {     public static void main(String[] args) {         int num = 200;
            if (num < 100) {
                System.out.println("100보다 작음");
                System.out.println("여기는 참");
            } else {
                System.out.println("100보다 작zma");
                System.out.println("여기는 거짓");
            }
    
            System.out.println("프로그램 끝!");
        }
    }

---
+++ src/main/java/Code05_04.java
@@ -0,0 +1,16 @@
+public class Code05_04 {
+    public static void main(String[] args) {
+        int num = 200;
+
+        if (num < 100) {
+            System.out.println("100보다 작음");
+            System.out.println("여기는 참");
+        } else {
+            System.out.println("100보다 작zma");
+            System.out.println("여기는 거짓");
+        }
+
+        System.out.println("프로그램 끝!");
+    }
+}
+
