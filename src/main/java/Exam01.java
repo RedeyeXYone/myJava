c967f8a45d7f9a54c130762d579373deae84abed
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-03-31 16:54:35 +0900

    import java.util.Scanner;
    public class Exam01 {
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
    
            String str = sc.nextLine();
    
            str = str.replaceAll(" ", "");
    
            str = str.toLowerCase();
    
            System.out.println(str);
        }
    }

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class Exam01 {
     public static void main(String[] args) {
-        String str = "Hello BYE";
+        Scanner sc = new Scanner(System.in);
+
+
+        String str = sc.nextLine();
+
 
         str = str.replaceAll(" ", "");
 
+
         str = str.toLowerCase();
 
-        System.out.println(str);
 
+        System.out.println(str);
     }
 }
