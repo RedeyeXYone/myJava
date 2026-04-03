dfc792f91941c8facc880ef92b72c6b64daa50f8
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-04-03 20:35:43 +0900

    public class Code04_05 {     public static void main(String[] args) {         int a = 200;         int b = 300;         int result = 500;
            System.out.printf("%d + %d", a, b);
            System.out.printf(" = %d ", result);
        }
    }

---
+++ src/main/java/Code04_05.java
@@ -0,0 +1,10 @@
+public class Code04_05 {
+    public static void main(String[] args) {
+        int a = 200;
+        int b = 300;
+        int result = 500;
+
+        System.out.printf("%d + %d", a, b);
+        System.out.printf(" = %d ", result);
+    }
+}
