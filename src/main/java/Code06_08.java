54da1901c7250bac3f21547d7a3d5f5639936686
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-04-10 14:26:57 +0900

    public class Code06_08 {     public static void main(String[] args) {         int hap = 0;         for (int i=1001 ; i<=2000 ; i+=2) {             hap += i;         }         System.out.println("1000부터 2000까지의 합계 : " + hap);
        }
    }

---
+++ src/main/java/Code06_08.java
@@ -0,0 +1,12 @@
+public class Code06_08 {
+    public static void main(String[] args) {
+        int hap = 0;
+        for (int i=1001 ; i<=2000 ; i+=2) {
+            hap += i;
+        }
+        System.out.println("1000부터 2000까지의 합계 : " + hap);
+
+    }
+}
+
+
