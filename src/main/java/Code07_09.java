fae8686a2bfdd98b761b4ab7a96693866d423c18
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-04-17 14:48:29 +0900

    class Rabbit3 { String shape; int xPos; int yPos;
    void setPosition(int x, int y) {
        xPos = x;
        yPos = y;
    }
    }
    
    public class Code07_09 {
        public static void main(String[] args){
            Rabbit rabbit1 = new Rabbit();
            Rabbit rabbit2 = new Rabbit();
            Rabbit rabbit3 = new Rabbit();
    
            rabbit1.shape = "원";
            rabbit2.shape = "삼각형";
            rabbit3.shape = "토끼";
    
            rabbit1.setPosition(100,100);
            rabbit1.setPosition(-100,100);
            rabbit1.setPosition(0,-100);
    
        }
    }

---
+++ src/main/java/Code07_09.java
@@ -0,0 +1,27 @@
+class Rabbit3 {
+String shape;
+int xPos;
+int yPos;
+
+void setPosition(int x, int y) {
+    xPos = x;
+    yPos = y;
+}
+}
+
+public class Code07_09 {
+    public static void main(String[] args){
+        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit();
+        Rabbit rabbit3 = new Rabbit();
+
+        rabbit1.shape = "원";
+        rabbit2.shape = "삼각형";
+        rabbit3.shape = "토끼";
+
+        rabbit1.setPosition(100,100);
+        rabbit1.setPosition(-100,100);
+        rabbit1.setPosition(0,-100);
+
+    }
+}
