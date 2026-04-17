7e3838d49f3bc94390b5c4e994dde7a77799daca
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-04-17 14:25:43 +0900

    class Rabbit1 {     String shape;     int xPos;     int yPos;
        void setPosition(int x, int y) {
            xPos = x;
            yPos = y;
        }
    }
    
    public class Code07_07 {
        public static void main(String[] args){
            Rabbit rabbit1 = new Rabbit();
            Rabbit rabbit2 = new Rabbit();
            Rabbit rabbit3 = new Rabbit();
        }
    }

---
+++ src/main/java/Code07_07.java
@@ -0,0 +1,19 @@
+class Rabbit1 {
+    String shape;
+    int xPos;
+    int yPos;
+
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+}
+
+public class Code07_07 {
+    public static void main(String[] args){
+        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit();
+        Rabbit rabbit3 = new Rabbit();
+    }
+}
+
