c49a296478b2afb59826406c0e48f25f4f9c36eb
RedeyeXYone <sunghyun.kim0914@gmail.com>
2026-04-20 14:08:21 +0900

    public class Exam03 {     public static void main(String[] args) {         int[] nums = {1, 3, 7, 1, 4, 7, 3, 1, 9, 0, 7, 3};         int i = 0;
            for ( i = 0 ; i <= nums[i] ; i++ ) {
                System.out.println(" ");
                if (i >= nums[i]) {
                    System.out.printf("출력결과 : %d입니다.", i);
                }
    
            }
        }
    }

---
+++ src/main/java/Exam03.java
@@ -0,0 +1,14 @@
+public class Exam03 {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 7, 1, 4, 7, 3, 1, 9, 0, 7, 3};
+        int i = 0;
+
+        for ( i = 0 ; i <= nums[i] ; i++ ) {
+            System.out.println(" ");
+            if (i >= nums[i]) {
+                System.out.printf("출력결과 : %d입니다.", i);
+            }
+
+        }
+    }
+}
